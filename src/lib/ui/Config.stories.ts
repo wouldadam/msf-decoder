b43d2aed71540ce9719dabc35bafc0a9709a5beb
Adam Wood <contact@adamwood.org>
2022-11-28 23:53:59 +0000

    test: Add storybook

---
+++ src/lib/ui/Config.stories.ts
@@ -0,0 +1,12 @@
+import Config from "./Config.svelte";
+
+export default {
+  title: "Config",
+  component: Config,
+};
+
+export const Standard = () => {
+  return {
+    Component: Config,
+  };
+};
