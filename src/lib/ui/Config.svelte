938c870fd805954647df27689d18d88707d556b3
Adam Wood <contact@adamwood.org>
2023-02-01 15:17:28 +0000

    feat: Add theme select

--- src/lib/ui/Config.svelte
+++ src/lib/ui/Config.svelte
@@ -20,6 +20,8 @@
   } from "../config";
   import { defaultProcessorKey, Processor } from "../processing/Processor";
 
+  import { themeChange } from "theme-change";
+
   let devices: MediaDeviceInfo[] | null = null;
   let audioFileInput: HTMLInputElement | null = null;
   $: if (!($audioSource instanceof File)) {
@@ -43,6 +45,8 @@
 
     const allDevices = await navigator.mediaDevices.enumerateDevices();
     devices = allDevices.filter((d) => d.kind === "audioinput");
+
+    themeChange(false);
   });
 
   const views: Array<{ mode: DisplayMode; tip: string; icon?: any }> = [
@@ -145,7 +149,7 @@
     <div class="flex flex-row grow gap-4 mt-2">
       <input
         value={formatTime(audioTime)}
-        class="input input-sm pointer-events-none text-right w-20"
+        class="input input-sm pointer-events-none text-right w-24"
       />
 
       <button
@@ -174,7 +178,7 @@
       <span>Hz</span>
     </label>
 
-    <div class="flex flex-row grow gap-2 mt-2">
+    <div class="flex flex-row grow gap-2 mt-2 justify-between">
       <div class="btn-group btn-horizontal">
         {#each views as view}
           <button
@@ -187,6 +191,33 @@
           </button>
         {/each}
       </div>
+
+      <select class="select select-sm" data-choose-theme>
+        <option value="default">Default</option>
+
+        <optgroup label="Light">
+          <option value="light">Light</option>
+          <option value="cupcake">Cupcake</option>
+          <option value="emerald">Emerald</option>
+          <option value="corporate">Corporate</option>
+          <option value="lofi">Lofi</option>
+        </optgroup>
+
+        <optgroup label="Dark">
+          <option value="dark">Dark</option>
+          <option value="halloween">Halloween</option>
+          <option value="dracula">Dracula</option>
+          <option value="night">Night</option>
+          <option value="retro">Retro</option>
+        </optgroup>
+
+        <optgroup label="Novelty">
+          <option value="cyberpunk">Cyberpunk</option>
+          <option value="wireframe">Wireframe</option>
+          <option value="synthwave">Synthwave</option>
+          <option value="aqua">Aqua</option>
+        </optgroup>
+      </select>
     </div>
   </div>
 </div>
